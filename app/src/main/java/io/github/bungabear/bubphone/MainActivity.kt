529d8df107b6fcff15cfeab90d978631d4794ac5
Minjae Son <jay.son@kt.com>
2019-04-09 15:15:59 +0900

    v1.0

--- app/src/main/java/io/github/bungabear/bubphone/MainActivity.kt
+++ app/src/main/java/io/github/bungabear/bubphone/MainActivity.kt
@@ -1,47 +1,36 @@
 package io.github.bungabear.bubphone
 
 import android.content.Intent
-import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Message
 import android.util.Log
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationManagerCompat
 import androidx.databinding.DataBindingUtil
 import com.google.android.material.snackbar.Snackbar
-import io.github.bungabear.bubphone.databinding.ActivityScrollingBinding
-import kotlinx.android.synthetic.main.activity_scrolling.*
+import io.github.bungabear.bubphone.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.ref.WeakReference
-import androidx.core.app.NotificationManagerCompat
 
 
-
-
-class ScrollingActivity : AppCompatActivity() {
-
-    private lateinit var mBinding : ActivityScrollingBinding
-//    private lateinit var etPw : EditText
+class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+    private lateinit var mBinding : ActivityMainBinding
+    //    private lateinit var etPw : EditText
     private val a by lazy { getString(R.string.app_name) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val securePreference = SecurePreferences(this, packageName, getString(R.string.large_text), true)
-        setContentView(R.layout.activity_scrolling)
-        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_scrolling)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         mHandler = MyHandler(this)
-        setContentView(R.layout.activity_scrolling)
-        setSupportActionBar(toolbar)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         mBinding.pw = ""
         mBinding.setSubmitListener {
+            Log.d(TAG, mBinding.pw)
             if(mBinding.pw.isNullOrBlank()){
                 Toast.makeText(this, "비밀번호를 입력해주세요", Toast.LENGTH_SHORT).show()
             }
@@ -81,23 +70,6 @@ class ScrollingActivity : AppCompatActivity() {
         }
         return false
     }
-//
-//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        menuInflater.inflate(R.menu.menu_scrolling, menu)
-//        return true
-//    }
-//
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//
-//        return when (item.itemId) {
-//            R.id.action_settings -> true
-//            else -> super.onOptionsItemSelected(item)
-//        }
-//    }
 
     fun handleMessage(msg : Message){
         val pw = "min6422!#@"
@@ -131,8 +103,8 @@ class ScrollingActivity : AppCompatActivity() {
         }
 
         private class MyHandler(
-            activity : ScrollingActivity,
-            val wrActivity : WeakReference<ScrollingActivity> = WeakReference(activity)
+                activity : MainActivity,
+                val wrActivity : WeakReference<MainActivity> = WeakReference(activity)
         )
             : Handler(){
             override fun handleMessage(msg: Message?) {
--- app/src/main/java/io/github/bungabear/bubphone/NotiListenService.kt
+++ app/src/main/java/io/github/bungabear/bubphone/NotiListenService.kt
@@ -9,98 +9,70 @@ import android.os.Looper
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 import android.util.Log
+import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 
 
 class NotiListenService : NotificationListenerService() {
     val TAG = NotiListenService::class.java.simpleName
     private val a by lazy { getString(R.string.app_name) }
-    override fun onListenerConnected() {
-        Log.d(TAG, "onListerConnected")
-        super.onListenerConnected()
-    }
-
-    override fun onListenerDisconnected() {
-        Log.d(TAG, "onListenerDisconnected")
-        super.onListenerDisconnected()
-    }
-
-    override fun onBind(intent: Intent?): IBinder? {
-        return super.onBind(intent)
-    }
-
-    override fun onCreate() {
-        Log.d(TAG, "onCreate")
-        super.onCreate()
-    }
-
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Log.d(TAG, "onStartCommand")
-        return Service.START_STICKY
-    }
-
-    override fun onDestroy() {
-        Log.d(TAG, "onDestroy")
-        super.onDestroy()
-    }
-
-    override fun onTaskRemoved(rootIntent: Intent?) {
-        Log.d(TAG, "onTaskRemoved")
-        super.onTaskRemoved(rootIntent)
-    }
-
-    override fun onLowMemory() {
-        Log.d(TAG, "onLowMemory")
-        super.onLowMemory()
-    }
 
     override fun onNotificationPosted(sbn: StatusBarNotification) {
         super.onNotificationPosted(sbn)
-        Log.v(TAG, "onNotificationPosted : ${sbn.notification.extras.bundle2String()}")
+//        Log.v(TAG, "onNotificationPosted : ${sbn.notification.extras.bundle2String()}")
+        val handler = Handler()
         val preference = SecurePreferences(this, packageName, getString(R.string.large_text), true)
         val pw = preference.getString(a)
         val pack = sbn.packageName
         val packageRegex = Regex(".*(sms|messag).*")
         val urlRegex = Regex("http\\S*")
         if(packageRegex.matches(pack)){
-            Log.d(TAG, "package filtered : $pack")
             val text = sbn.notification.extras["android.text"]?.toString()?:""
             if(text.contains("[법인명의 본인확인]")){
-                Log.d(TAG, "text filtered : $text")
                 val result = urlRegex.find(text)
                 result?.let {
                     val url = result.value
-                    Thread{
-                        Handler(Looper.getMainLooper()).post {
-                            val hiddenWebview = WebView(this)
-                            hiddenWebview.settings.javaScriptEnabled = true
-                            hiddenWebview.webViewClient = object : WebViewClient(){
-                                var isLoaded = false
-                                var isPwinjected = false
-                                override fun onPageFinished(view: WebView?, url: String?) {
-                                    Log.d("activity", "onPageFinished $url")
-                                    Handler().postDelayed({
-                                        view?.evaluateJavascript("javascript:document.getElementById('bubpw').value='$pw';")
-                                        {
-                                            Log.d("webview", "pw fill end")
-                                            view.evaluateJavascript("javascript:document.getElementById('fmBtn').click();")
-                                            { s->
-                                                //div class중 iptOk가 있어야함.
-                                                //body.wrap(main_content.ipt.iptOk)
-                                                //javascript interface로 html을 100ms정도 delay시켜 받아와 체크.
-                                                Log.d("webview", "click end : $s, ${hiddenWebview}")
-                                            }
+                    Thread{ Handler(Looper.getMainLooper()).post {
+                        val hiddenWebview = WebView(this)
+                        hiddenWebview.addJavascriptInterface(JavascriptCallback(), "android")
+                        hiddenWebview.settings.javaScriptEnabled = true
+                        hiddenWebview.webViewClient = object : WebViewClient(){
+                            override fun onPageFinished(view: WebView?, url: String?) {
+                                handler.postDelayed({
+                                    view?.evaluateJavascript("javascript:document.getElementById('bubpw').value='$pw';")
+                                    {
+                                        view.evaluateJavascript("javascript:document.getElementById('fmBtn').click();")
+                                        { s->
+                                            handler.postDelayed({
+                                                view.loadUrl("javascript:window.android.loginCheck(document.getElementsByTagName('body')[0].innerHTML)")
+                                            }, 100)
                                         }
-                                    }, 1000)
-                                }
+                                    }
+                                }, 1000)
                             }
-                            hiddenWebview.loadUrl(url)
                         }
+                        hiddenWebview.loadUrl(url)
+                    }
                     }.start()
                 }
             }
         }
+    }
+
+    inner class JavascriptCallback{
+
+        @JavascriptInterface
+        fun loginCheck(body : String){
+            if(body.contains("iptOk")){
+                // 인증 성공
+            }
+            else {
+                Toast.makeText(this@NotiListenService, "법폰 : 비밀번호가 틀린거같아요...", Toast.LENGTH_LONG).show()
+            }
+        }
+
     }
 
     fun Bundle.bundle2String() = this.keySet().joinToString { "$it : ${this.get(it)}" }
